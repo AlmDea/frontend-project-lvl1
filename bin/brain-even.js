116a8dafac5ab179c3a3fe6524e12d215f0c97af
Irina S <inspired.by.summer@yandex.ru>
2022-02-11 22:15:52 +0300

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
 
-const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (number) => (number % 2 === 0);
+import brainEven from '../src/games/game-even.js';
 
-const gameQuestionAnswer = () => {
-  const startNumber = 1;
-  const maxNumberForGame = 100;
-  const question = generateRandom(startNumber, maxNumberForGame);
-  const answer = isEven(question) ? 'yes' : 'no';
-  return [question, answer];
-};
-
-const brainEven = () => newGame(gameQuestion, gameQuestionAnswer);
-export default brainEven;
+brainEven;
---
+++ src/games/game-even.js
@@ -0,0 +1,16 @@
+import newGame from '../index.js';
+import generateRandom from '../generateRandom.js';
+
+const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => (number % 2 === 0);
+
+const gameQuestionAnswer = () => {
+  const startNumber = 1;
+  const maxNumberForGame = 100;
+  const question = generateRandom(startNumber, maxNumberForGame);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const brainEven = () => newGame(gameQuestion, gameQuestionAnswer);
+export default brainEven;
